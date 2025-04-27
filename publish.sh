c9502518bffc9dcb4a6a76b7466836bf6e8b8f14
Brandt Keller <brandt.keller@defenseunicorns.com>
2025-04-27 20:20:00 +0000

    fix: look for breaking changes
    Signed-off-by: Brandt Keller <brandt.keller@defenseunicorns.com>

--- publish.sh
+++ publish.sh
@@ -5,7 +5,7 @@ echo "$CF_PAGES_BRANCH"
 if [ "$CF_PAGES_BRANCH" == "main" ]; then
   # Set baseurl to the production URL
   echo "Running the production publish"
-  hugo -b missionfocused.dev
+  hugo -b https://missionfocused.dev
 else
   # Else set it to the expected dev url
   echo "Running the development preview publish"
